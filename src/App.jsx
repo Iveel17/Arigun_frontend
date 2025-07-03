6ead801a0f5fd26606136158e79f862487308701
Munkh-Iveel <iveelv968@gmail.com>
2025-07-03 19:58:30 +0800

    route path okay for now

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ProductsPage from './pages/ProductsPage/ProductsPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import CartPage from './pages/CartPage/CartPage';
+import CoursePage from './pages/CoursesPage/CoursePage/CoursePage';
 
 import './App.css';
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="cart" element={<CartPage />} />
+        <Route path="/courses/:id" element={<CoursePage />} />
         {/* Add more routes here as needed */}        
       </Routes>
     </Router>
